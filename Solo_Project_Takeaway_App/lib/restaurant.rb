5fef25dbaa16faa8d2876f63ea4a38ddd227710b
Iain <iain.hoolahan@gmail.com>
2022-10-21 17:04:05 +0100

    committing progres - first user story tested

--- Solo_Project_Takeaway_App/lib/restaurant.rb
+++ Solo_Project_Takeaway_App/lib/restaurant.rb
@@ -2,26 +2,52 @@
 
 class Restaurant
   def initialize
-    # menu as a pre-populated array
-    # order as an empty array
-    # dish 
+    # @menu = [["#1", "pizza", 10], ["#2", "chips", 5], ["#3", "hot dog", 7]]
+    @menu = ["#01 - pizza - £10.00", "#02 - chips - £5.00", "#03 - hot dog - £7.00"]
+    @order = []
   end
 
   def show_menu
-    # prints the menu
+    @menu.each do |item|
+      puts item
+    end
+
+    #@menu.each do |items|
+     # items.each do |number, item, price|
+        # print number, item, price, "\n"
+     # end
+    #end
   end
 
   def add(dish_number) 
+    # write failing case for index with nil
+    @order << @menu[dish_number - 1]
     # pushes item from menu array to order array
     # Returns nothing
   end
 
   def order
+    return @order
     # Returns order array
   end
 
+  def total
+  #puts @order.map {|x| x[/\d+/]}
+  values = [] 
+  @order.join(' ').split(' ').each do |item|
+    item.delete unless item.include? ('£')
+  end
+  end
+
   def bill
+
     # prints order array with total at the bottom
   end
 
 end
+
+test = Restaurant.new
+test.show_menu
+test.add(1)
+test.add(2)
+test.total
--- Solo_Project_Takeaway_App/spec/restaurant_spec.rb
+++ Solo_Project_Takeaway_App/spec/restaurant_spec.rb
@@ -6,7 +6,33 @@ describe Restaurant do
   context 'when the show_menu method is called' do
     it 'prints the menu' do
       menu = Restaurant.new
-      expect(menu.show_menu).to eq [["#1", "pizza", 10], ["#2", "chips", 5], ["#3", "hot dog", 7]]
+      expect(menu.show_menu).to eq ["#01 - pizza - £10.00", "#02 - chips - £5.00", "#03 - hot dog - £7.00"]
+    end
+  end
+  context 'when a dish is added' do
+    it 'populates the order array' do
+      menu = Restaurant.new
+      new_order = Restaurant.new
+      new_order.add(1)
+      expect(new_order.order).to eq ["#01 - pizza - £10.00"]
+    end
+  end
+  context 'when multiple dishes are added' do
+    it 'populates the order array' do
+      menu = Restaurant.new
+      new_order = Restaurant.new
+      new_order.add(1)
+      new_order.add(2)
+      expect(new_order.order).to eq ["#01 - pizza - £10.00", "#02 - chips - £5.00"]
+    end
+  end
+  context 'when the bill method is called' do
+    it 'prints an itemised order with a total at the end' do
+      menu = Restaurant.new
+      new_order = Restaurant.new
+      new_order.add(1)
+      new_order.add(2)
+      expect(new_order.total).to eq 15
     end
   end
 end
